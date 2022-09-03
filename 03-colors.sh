f1aac9ec05fe49e7b3a915eb6d81c996b8ad40c7
mvpc-11232 <mvp2612@hotmail.com>
2022-09-03 07:27:43 +0530

    hai hello

--- 03-colors.sh
+++ 03-colors.sh
@@ -2,5 +2,5 @@
 
 #Syntax : echo -e  "\e[ColorCodem Your Tex \e[0m"
 
-echo -e "\e[37m DevOps has a very great future \e[0m"
+echo -e "\e[33m DevOps has a very great future \e[0m"
 echo -e "\e[43;37m DevOps has a very great future \e[0m"
