c15870481ff78bb679f8078a71aaf5b8531c2f2e
mvpc-11232 <mvp2612@hotmail.com>
2022-09-05 06:34:37 +0530

    Auto Push Commit

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -3,7 +3,7 @@
 #  Here are the special variables 
 
 #  $0          : This will gives the name of the script you're running 
-#  $1 to $9    :
+#  $1 to $9    :You can pass a maximum of 9 variables from the command line when you're running the script 
 #  $* 
 #  $@ 
 #  $# 
