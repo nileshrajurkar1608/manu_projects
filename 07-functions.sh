2ea1bfa1544f5f48955a02415125166ebf91e734
mvpc-11232 <mvp2612@hotmail.com>
2022-09-05 07:05:50 +0530

    Auto Push Commit

--- 07-functions.sh
+++ 07-functions.sh
@@ -8,9 +8,9 @@
 
 # sample 
 
-
+# Defining the function stat
 stat() {
     echo "Load Average on the system from last 1 min is : $(uptime | awk -F : '{print $NF}' | awk -F , '{print $1}')"
     echo "Number of users signed in is : $(who|wc -l)" 
-    echo"
+    echo "Stat Function is completed bye"
 }
