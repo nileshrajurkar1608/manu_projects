dd46936fada654604e695ebfbdeffbbacaf485fd
mvpc-11232 <mvp2612@hotmail.com>
2022-09-05 07:31:34 +0530

    Auto Push Commit

--- 03-colors.sh
+++ 03-colors.sh
@@ -3,7 +3,7 @@
 #Syntax : echo -e  "\e[ColorCodem Your Tex \e[0m"
 
 # Syntax is 
-# echo -e "\e[32m  TextOfYourChoice  \e[0m"
+# echo -e "\e[32m TextOfYourChoice  \e[0m"
 
 echo -e "\e[33m DevOps has a very great future \e[0m"
 echo -e "\e[43;37m DevOps has a very great future \e[0m"
