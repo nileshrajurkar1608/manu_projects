de0d803ac8b76d40ddd07539d41451b8844f43fd
mvpc-11232 <mvp2612@hotmail.com>
2022-09-05 07:04:26 +0530

    Auto Push Commit

--- 07-functions.sh
+++ 07-functions.sh
@@ -1,10 +1,9 @@
 #!/bin/bash 
 
-sample() {
-    echo "hai from the sample function"
-    echo "I will be printing the uptime"
-    uptime  
-}
+# sample() {
+#     echo "hai from the sample function"
+#     echo "I will be printing the uptime"
+#     uptime  
+# }
 
-
-sample 
+# sample 
