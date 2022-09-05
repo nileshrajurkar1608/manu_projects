4267dca497629b4ce3cd8fc63b7f86f09a8dc50f
mvpc-11232 <mvp2612@hotmail.com>
2022-09-05 06:44:01 +0530

    Auto Push Commit

--- 05-special-vars.sh
+++ 05-special-vars.sh
@@ -5,7 +5,7 @@
 #  $0          : This will gives the name of the script you're running 
 #  $1 to $9    :You can pass a maximum of 9 variables from the command line when you're running the script 
 #  $* 
-#  $@ 
+#  $@      
 #  $# 
 #  $$ 
 echo -e "script name that you're running is : \e[32m $0 \e[0m"
