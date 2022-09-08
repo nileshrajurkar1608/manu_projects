dbd05ef39a47f0c8c90519ba049b0e2a104bfa7e
mvpc-11232 <mvp2612@hotmail.com>
2022-09-08 07:00:54 +0530

    Auto Push Commit

--- 11-if-else.sh
+++ 11-if-else.sh
@@ -27,5 +27,6 @@ if [ "$ACTION" = "start" ] ; then
         echo "Restarting XYZ Service" 
         exit 2
     else 
-        echo -e "Valid options are \e[31m start or stop or restart only \e[0m"
+        echo -e "Valid options are \e[31m start or stop or restart only \e[0m" 
+        exit 3
 fi 
