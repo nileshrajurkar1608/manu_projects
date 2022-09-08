2fc43d430438f8a1a3b8b6148cd33a850ff64d30
mvpc-11232 <mvp2612@hotmail.com>
2022-09-08 06:51:43 +0530

    Auto Push Commit

--- 11-if-else.sh
+++ 11-if-else.sh
@@ -20,7 +20,7 @@ if [ "$ACTION" = "start" ] ; then
     echo "Starting XYZ Service"
 
 elif ["$ACTION" = "start"]; then 
-     echo "Starting XYZ Service"
+     echo "Stopping XYZ Service"
 
 else 
     echo "Only valid option is start"
