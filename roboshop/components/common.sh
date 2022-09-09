68ff173aa81a68ee393f920e66ae4bb9ef02d9d9
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 07:22:01 +0530

    Auto Push Commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -7,7 +7,7 @@ fi
 
 # Declaring the stat function
 stat() {
-    if [ $? -eq 0 ] ; then 
+    if [ $1 -eq 0 ] ; then 
         echo -e "\e[32m Success \e[0m" 
     else
         echo -e "\e[31m Failure. Look for the logs \e[0m"  
