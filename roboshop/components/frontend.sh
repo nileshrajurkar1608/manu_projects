4e7f807e93ce411e6602fa3316b342af165cf880
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 06:31:47 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,7 +3,7 @@
 ID=$(id -u)
 if [ $ID -eq 0 ]; then 
     echo "Executing httpd installation"
-    yum install httpd -y  
+     
 else 
     echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
     exit 1
