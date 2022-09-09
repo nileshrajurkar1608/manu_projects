59828b9b33f60ff5b064c6f9e1a510ea070c12f9
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 06:52:33 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -2,7 +2,7 @@
 
 source components/common.sh
 
-yum install nginx -y   >
+yum install nginx -y   >> /tmp/frontend.log
 systemctl enable nginx 
 systemctl start nginx 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
