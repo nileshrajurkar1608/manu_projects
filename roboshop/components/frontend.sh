727ce06c7d517cbab8759b20d2773fdf9bd1ac92
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 06:31:12 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+ID=$(id -u)
+if [ $ID -eq 0 ]; then 
+    echo "Executing httpd installation"
+    yum install httpd -y  
+else 
+    echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
+    exit 1
+fi 
+
 yum install nginx -y 
 systemctl enable nginx 
 systemctl start nginx 
