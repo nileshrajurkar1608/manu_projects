5356525807a44a3ecdf380b1afe5d2613e1ffad1
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 06:40:33 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-ID=$(id -u)
-if [ $ID -ne 0 ]; then 
-    echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
-    exit 1
-fi 
+
 
 yum install nginx -y 
 systemctl enable nginx 
