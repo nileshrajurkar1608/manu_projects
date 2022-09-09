b272e0da8893ffe2e66c114097af9ec2df0c4c52
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 06:26:28 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,13 +4,3 @@ yum install nginx -y
 systemctl enable nginx 
 systemctl start nginx 
  curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
- 
-
-Let's download the HTDOCS content and deploy it under the Nginx path.
-
-```
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
-```
-
-Deploy in Nginx Default Location.
