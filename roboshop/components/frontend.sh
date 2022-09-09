d9fd01d3c03e0bdab094d155b50cf00d7e9bb05a
mvpc-11232 <mvp2612@hotmail.com>
2022-09-09 06:25:44 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "I am installing frontend
+
+Let's download the HTDOCS content and deploy it under the Nginx path.
+
+```
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+```
+
+Deploy in Nginx Default Location.
