e669f86efe7bdbdf175b33728b3afdfae0df3417
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 06:22:57 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -13,7 +13,7 @@ yum install -y mongodb-org
 stat $? 
 
 echo "Updating the $COMPONENT Config:"
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
 
 systemctl enable mongod >> /tmp/${COMPONENT}.log
 systemctl start mongod  
