d61ef951511c5dfc5e990a460712eed58c6a9898
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 06:42:02 +0530

    Auto Push Commit

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -6,30 +6,30 @@ source components/common.sh
 COMPONENT=catalogue
 
 
-# curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-# yum install nodejs -y
+# # curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+# # yum install nodejs -y
 
 
-# useradd roboshop
+# # useradd roboshop
 
-$ curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
-$ cd /home/roboshop
-$ unzip /tmp/catalogue.zip
-$ mv catalogue-main catalogue
-$ cd /home/roboshop/catalogue
-$ npm install
+# $ curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+# $ cd /home/roboshop
+# $ unzip /tmp/catalogue.zip
+# $ mv catalogue-main catalogue
+# $ cd /home/roboshop/catalogue
+# $ npm install
 
 
-vim systemd.servce
+# vim systemd.servce
 
-# mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-# systemctl daemon-reload
-# systemctl start catalogue
-# systemctl enable catalogue
-# systemctl status catalogue -l
+# # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+# # systemctl daemon-reload
+# # systemctl start catalogue
+# # systemctl enable catalogue
+# # systemctl status catalogue -l
 
-NOTE: You should see the log saying `connected to MongoDB`, then only your catalogue
-will work and can fetch the items from MongoDB
+# NOTE: You should see the log saying `connected to MongoDB`, then only your catalogue
+# will work and can fetch the items from MongoDB
 
-Ref Log:
-{"level":"info","time":1656660782066,"pid":12217,"hostname":"ip-172-31-13-123.ec2.internal","msg":"MongoDB connected","v":1}
+# Ref Log:
+# {"level":"info","time":1656660782066,"pid":12217,"hostname":"ip-172-31-13-123.ec2.internal","msg":"MongoDB connected","v":1}
