510cdbcd4db1ab1fbe9db8bb28edb3ef734b2350
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 06:51:11 +0530

    Auto Push Commit

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -25,6 +25,7 @@ stat $?
 
 echo -n "Cleanup of Old $COMPONENT content:"
 rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log 
+stat $?
 
 echo -n "Extracting $COMPONENT content: "
 cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
