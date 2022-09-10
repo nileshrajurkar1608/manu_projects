6354b1bc3cfcfdd530aeb3e3c70a2bfd26d6ea8f
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 06:14:32 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -8,7 +8,7 @@ echo -n "Configuring the MongoDB repo:"
 curl -s -o /etc/yum.repos.d/${COMPONENT}.repo https://raw.githubusercontent.com/stans-robot-project/${COMPONENT}/main/mongo.repo
 stat $? 
 
-echo -n "Installing Monogdb:"
+echo -n "Installing ${COMPONENT}:"
 yum install -y mongodb-org 
 stat $? 
 
