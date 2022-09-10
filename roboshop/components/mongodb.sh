43df287dfc3c1cbc4db2101db5307a32d568ff07
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 06:26:37 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -12,7 +12,7 @@ echo -n "Installing ${COMPONENT}:"
 yum install -y mongodb-org >> /tmp/${COMPONENT}.log
 stat $? 
 
-echo "Updating the $COMPONENT Config:"
+echo -n "Updating the $COMPONENT Config:"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf 
 stat $? 
 
