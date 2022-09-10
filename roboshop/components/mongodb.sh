c3e672d91e90ffe2eeca5562cee6850ae13a3f54
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 06:34:58 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -26,7 +26,7 @@ curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/${COMPONE
 stat $?
 
 echo -n "Extracting the $COMPONENT Schema:"
-cd /tmp && unzip mongodb.zip && cd mongodb-main
+cd /tmp && unzip mongodb.zip
 stat $? 
 
 echo -n "Injecting the $COMPONENT schema: "
