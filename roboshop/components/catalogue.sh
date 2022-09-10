74d944f7ba71f450e1596bb6b6a0bccb1f81f99e
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 06:58:46 +0530

    Auto Push Commit

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -32,7 +32,7 @@ cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log
 unzip -o  /tmp/{COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
 stat $? 
 
-echo -n "Changing the "
+echo -n "Changing the ownership"
 
 echo -n "Installing $COMPONENT Dependencies:"
 npm install >> /tmp/${COMPONENT}.log 
