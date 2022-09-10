af9c864a3ffb593c36795090b96bc453e5e81d4a
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 07:20:17 +0530

    Auto Push Commit

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -50,18 +50,3 @@ systemctl daemon-reload  &>> /tmp/${COMPONENT}.log
 systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
 systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log
 stat $?
-
-
-# vim systemd.servce
-
-# # mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
-# # systemctl daemon-reload
-# # systemctl start catalogue
-# # systemctl enable catalogue
-# # systemctl status catalogue -l
-
-# NOTE: You should see the log saying `connected to MongoDB`, then only your catalogue
-# will work and can fetch the items from MongoDB
-
-# Ref Log:
-# {"level":"info","time":1656660782066,"pid":12217,"hostname":"ip-172-31-13-123.ec2.internal","msg":"MongoDB connected","v":1}
