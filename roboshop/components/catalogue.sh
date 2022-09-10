1754bf226273783cf1bf04c99784c8a83876e770
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 06:41:34 +0530

    Auto Push Commit

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -11,3 +11,12 @@ COMPONENT=catalogue
 
 
 # useradd roboshop
+
+$ curl -s -L -o /tmp/catalogue.zip "https://github.com/stans-robot-project/catalogue/archive/main.zip"
+$ cd /home/roboshop
+$ unzip /tmp/catalogue.zip
+$ mv catalogue-main catalogue
+$ cd /home/roboshop/catalogue
+$ npm install
+
+
