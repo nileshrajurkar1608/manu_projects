13639a3aeb657d2c8fd9efc6f7482e61be0dc399
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 07:19:41 +0530

    Auto Push Commit

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -23,11 +23,11 @@ echo -n "Downloading ${COMPONENT} :"
 curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >> /tmp/${COMPONENT}.log 
 stat $? 
 
-echo -n "Cleanup of Old ${COMPONENT content:"
+echo -n "Cleanup of Old ${COMPONENT} content:"
 rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log 
 stat $?
 
-echo -n "Extracting $COMPONENT content: "
+echo -n "Extracting ${COMPONENT content: "
 cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
 unzip -o  /tmp/{COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
 stat $? 
