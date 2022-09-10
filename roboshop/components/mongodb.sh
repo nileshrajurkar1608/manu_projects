7c65b9677e485d1741f4098a9c2740e63621c972
mvpc-11232 <mvp2612@hotmail.com>
2022-09-10 06:32:23 +0530

    Auto Push Commit

--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -30,6 +30,6 @@ cd /tmp && unzip mongodb.zip && cd mongodb-main
 stat $? 
 
 echo -n "Injecting the $COMPONENT schema: "
-mongo < catalogue.js
-mongo < users.js 
+mongo < catalogue.js && mongo < users.js
+ 
 stat $? 
