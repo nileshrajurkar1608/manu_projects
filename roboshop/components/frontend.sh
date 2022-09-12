1e99a3e04d1bb6d7ab83d0701c11e001bfef86fe
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 06:39:02 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -32,7 +32,7 @@ echo -n "Configuring the Reverse Proxy: "
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
-for i
+for component
 
 echo -n "Starting Ngnix: "
 systemctl restart nginx
