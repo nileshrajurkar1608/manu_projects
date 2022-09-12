7dc86bfb96d3996aaee98d0117370bd2dd55a1a4
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 06:52:54 +0530

    Auto Push Commit

--- roboshop/components/cart.sh
+++ roboshop/components/cart.sh
@@ -29,7 +29,7 @@ stat $?
 
 echo -n "Extracting ${COMPONENT} content: "
 cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
-unzip -o  /tmp/{COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
+unzip -o  /tmp/${COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
 stat $? 
 
 echo -n "Changing the ownership to ${FUSER}:"
