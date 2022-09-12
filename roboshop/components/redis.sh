b31d27a2b968b954632d5a1c5bd8321eeff34167
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 06:34:19 +0530

    Auto Push Commit

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -20,7 +20,7 @@ sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis/redis.conf
 stat $? 
 
 echo -n "Staring $COMPONENT: "
-systemctl daemon
+systemctl daemon-reload 
 systemctl enable redis &>> /tmp/${COMPONENT}.log
 systemctl restart redis &>> /tmp/${COMPONENT}.log
 stat $? 
