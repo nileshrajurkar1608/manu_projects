747534874272070750f1574d04bcd959eec798b8
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 07:03:01 +0530

    Auto Push Commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -24,6 +24,26 @@ USER_SETUP() {
 }
 
 
+DOWNLOAD_AND_EXTRACT() {
+    echo -n "Downloading ${COMPONENT} :"
+    curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >> /tmp/${COMPONENT}.log 
+    stat $? 
+
+    echo -n "Cleanup of Old ${COMPONENT} content:"
+    rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log 
+    stat $?
+
+    echo -n "Extracting ${COMPONENT} content: "
+    cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
+    unzip -o  /tmp/{COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
+    stat $? 
+
+    echo -n "Changing the ownership to ${FUSER}:"
+    chown -R $FUSER:$FUSER $COMPONENT/
+    stat $?
+
+} 
+
 NODEJS() {
     echo -n "Configure Yum Remos for nodejs:"
     curl -sL https://rpm.nodesource.com/setup_lts.x | bash >> /tmp/${COMPONENT}.log 
