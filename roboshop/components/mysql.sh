1fb85cf47abae32ce88a9275b76a2e3a363eb70b
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 07:23:12 +0530

    Auto Push Commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -8,7 +8,7 @@ COMPONENT=mysql
 LOGFILE=/tmp/robot.log 
 
 echo -n "Configuring the $COMPONENT repo: "
-curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/${COMPONENT}.repo $>> ${LOGFILE}
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/${COMPONENT}.repo >> ${LOGFILE}
 stat $? 
 
 echo -n "Installing $COMPONENT :"
