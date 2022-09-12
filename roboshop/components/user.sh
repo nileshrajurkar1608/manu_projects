3c03ea5321396ad8576cb4fd4d9de3ff08a71d93
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 06:21:54 +0530

    Auto Push Commit

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -6,3 +6,36 @@ source components/common.sh
 
 COMPONENT=user
 FUSER=roboshop 
+
+echo -n "Configure Yum Remos for nodejs:"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash >> /tmp/${COMPONENT}.log 
+stat $?
+
+echo -n "Installing nodejs:"
+yum install nodejs -y >> /tmp/${COMPONENT}.log 
+stat $? 
+
+echo -n "Adding $FUSER user:"
+id ${FUSER} >> /tmp/${COMPONENT}.log  || useradd ${FUSER}   # Creates users only in case if the user account doen's exist
+stat $? 
+
+echo -n "Downloading ${COMPONENT} :"
+curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" >> /tmp/${COMPONENT}.log 
+stat $? 
+
+echo -n "Cleanup of Old ${COMPONENT} content:"
+rm -rf /home/${FUSER}/${COMPONENT}  >> /tmp/${COMPONENT}.log 
+stat $?
+
+echo -n "Extracting ${COMPONENT} content: "
+cd /home/${FUSER}/ >> /tmp/${COMPONENT}.log 
+unzip -o  /tmp/{COMPONENT}.zip  >> /tmp/${COMPONENT}.log   &&   mv ${COMPONENT}-main ${COMPONENT} >> /tmp/${COMPONENT}.log 
+stat $? 
+
+echo -n "Changing the ownership to ${FUSER}:"
+chown -R $FUSER:$FUSER $COMPONENT/
+stat $?
+
+echo -n "Installing $COMPONENT Dependencies:"
+cd $COMPONENT && npm install &>> /tmp/${COMPONENT}.log 
+stat $? 
