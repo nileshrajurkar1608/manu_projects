06f2b62e868917e7792b5061b3784d31937af5f4
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 07:21:55 +0530

    Auto Push Commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -15,6 +15,6 @@ echo -n "Installing $COMPONENT :"
 yum install mysql-community-server -y $>> ${LOGFILE}
 stat $? 
 
-echo -n "Starting "
+echo -n "Starting ${COMPONENT} : "
 systemctl enable mysqld 
 systemctl start mysqld
