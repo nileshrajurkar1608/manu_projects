49ffb1cdb8c85798737a9db1cc5a972b0dfc62bc
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 06:26:58 +0530

    Auto Push Commit

--- roboshop/components/user.sh
+++ roboshop/components/user.sh
@@ -43,7 +43,7 @@ stat $?
 
 echo -n "Setup Systemd File"
 sed -i -e 's/REDIS_ENDPOINT/redis.roboshop.internal/'  -e 's/MONGO_ENDPOINT/mongodb.roboshop.internal/' /home/roboshop/user/systemd.service
-mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/catalogue.service
+mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
 
 
 Update `REDIS_ENDPOINT` with Redis Server IP
