ddbcbe74126e8b681138781cf0b68d16526eb6c2
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 07:19:11 +0530

    Auto Push Commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -5,3 +5,5 @@ if [ $ID -ne 0 ]; then
     echo -e "\e[31m Try executing the script with sudo or a root user \e[0m"
     exit 1
 fi 
+
+curl -s -L -o /etc/yum.repos.d/mysql.repo https://raw.githubusercontent.com/stans-robot-project/mysql/main/mysql.repo
