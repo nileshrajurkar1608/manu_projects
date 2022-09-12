231d15f9131f5118c3dea0859b294e6f8fd68be0
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 06:44:05 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -33,7 +33,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 for component in catalogue user ; do 
-sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" 
+sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
 
 echo -n "Starting Ngnix: "
 systemctl restart nginx
