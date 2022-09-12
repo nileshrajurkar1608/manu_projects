d9d507052df13e3256feb85337d871a246b705e0
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 06:18:39 +0530

    Auto Push Commit

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -18,7 +18,7 @@ echo -n "Whitelisting the redis config : "
 sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf 
 stat $? 
 
-echo -n "Start $COMPONENT"
+echo -n "Staring $COMPONENT"
 
 # # curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
 # # yum install redis-6.2.7 -y
