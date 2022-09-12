2391822f5d9b4a9d10c05f75d1970359e359709b
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 07:06:10 +0530

    Auto Push Commit

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -44,16 +44,16 @@ DOWNLOAD_AND_EXTRACT() {
 } 
 
 CONFIG_SVC() {
-echo -n "Configuring the Systemd file: "
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${FUSER}/${COMPONENT}/systemd.service 
-mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-stat $? 
+    echo -n "Configuring the Systemd file: "
+    sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${FUSER}/${COMPONENT}/systemd.service 
+    mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
+    stat $? 
 
-echo -n "Starting the service"
-systemctl daemon-reload  &>> /tmp/${COMPONENT}.log 
-systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
-systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log
-stat $?
+    echo -n "Starting the service"
+    systemctl daemon-reload  &>> /tmp/${COMPONENT}.log 
+    systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
+    systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log
+    stat $?
 }
 
 NODEJS() {
