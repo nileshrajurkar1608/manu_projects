e0da3ab026cc74532fd1404f860970f6fc051578
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 06:14:53 +0530

    Auto Push Commit

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -7,7 +7,7 @@ source components/common.sh
 COMPONENT=redis
 
 echo -n "Configuring the $COMPONENT repo: "
-curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
+curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo  &>> /tmp/${COMPONENT}.log 
 stat $?
 
 echo -n "Installing $COMPONENT: "
