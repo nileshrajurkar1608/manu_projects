e6771f2171ec6d407017ec1566814227caed614a
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 06:11:32 +0530

    Auto Push Commit

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -11,7 +11,8 @@ curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.r
 stat $?
 
 echo -n "Installing $COMPONENT: "
-
+yum install redis-6.2.7 -y
+stat $? 
 
 # curl -L https://raw.githubusercontent.com/stans-robot-project/redis/main/redis.repo -o /etc/yum.repos.d/redis.repo
 # yum install redis-6.2.7 -y
