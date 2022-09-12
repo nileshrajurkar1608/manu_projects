1e019f76cfcc95b7821360db10780fa5c5d5e655
mvpc-11232 <mvp2612@hotmail.com>
2022-09-12 07:05:42 +0530

    Auto Push Commit

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -11,15 +11,6 @@ NODEJS
 
 
 CONFIG_SVC 
-echo -n "Configuring the Systemd file: "
-sed -i -e 's/MONGO_DNSNAME/mongodb.roboshop.internal/' /home/${FUSER}/${COMPONENT}/systemd.service 
-mv /home/${FUSER}/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service
-stat $? 
 
-echo -n "Starting the service"
-systemctl daemon-reload  &>> /tmp/${COMPONENT}.log 
-systemctl enable ${COMPONENT} &>> /tmp/${COMPONENT}.log
-systemctl start ${COMPONENT} &>> /tmp/${COMPONENT}.log
-stat $?
 
 
