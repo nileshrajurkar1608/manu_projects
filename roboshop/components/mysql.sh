3c512c3980697f3389b38222fec9fdd09fb7e06a
mvpc-11232 <mvp2612@hotmail.com>
2022-09-13 06:56:14 +0530

    Auto Push Commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -30,7 +30,7 @@ stat $?
 echo show databases | mysql -uroot -pRoboShop@1 &>> ${LOGFILE}
 if [ $? -ne 0 ]; then 
     echo -n "Reset Root Password: "
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password  -uroot -p"${DEFAULT_ROOT_PASSWORD}" &>> ${LOGFILE}
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password  -uroot -p"${DEFAULT_ROOT_PASSWORD}" > ${LOGFILE}
     stat $? 
 fi 
 
