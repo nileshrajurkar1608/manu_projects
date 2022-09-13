d49238e0529076baf617b4a5660da9cbbcd1c9e6
mvpc-11232 <mvp2612@hotmail.com>
2022-09-13 07:25:53 +0530

    Auto Push Commit

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -33,7 +33,7 @@ mv localhost.conf /etc/nginx/default.d/roboshop.conf
 stat $?
 
 
-for component in catalogue user cart; do 
+for component in catalogue user cart shipping; do 
     echo -n "Updating the proxy file"
     sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/"  /etc/nginx/default.d/roboshop.conf
     stat $?
