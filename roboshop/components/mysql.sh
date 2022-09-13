175fdd65b20410ced902e30f5c3f07bd1a6d4e3b
mvpc-11232 <mvp2612@hotmail.com>
2022-09-13 06:17:24 +0530

    Auto Push Commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -24,7 +24,3 @@ stat $?
 echo -n "Fetching the default root password: "
 DEFAULT_ROOT_PASSWORD=$(sudo grep temp /var/log/mysqld.log | head -n 1 | awk -F " " '{print $NF}')
 stat $? 
-
-
-
-if( abc = bed ) {}
