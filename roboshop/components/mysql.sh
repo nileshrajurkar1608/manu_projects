339d85c985485ac529a7d4434625f35822d8a37e
mvpc-11232 <mvp2612@hotmail.com>
2022-09-13 06:38:43 +0530

    Auto Push Commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -25,7 +25,7 @@ echo -n "Fetching the default root password: "
 DEFAULT_ROOT_PASSWORD=$(sudo grep temp /var/log/mysqld.log | head -n 1 | awk -F " " '{print $NF}')
 stat $? 
 
-echo show databases | mysql -uroot -pRoboShop@1 &>> ${LOGFILE}
+echo show databases | mysql -uroot -p"" &>> ${LOGFILE}
 if [ $? -eq 0 ]; then 
     echo -n "Reset Root Password: "
     echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1';" | mysql --connect-expired-password  -uroot -p"${DEFAULT_ROOT_PASSWORD}" &>> ${LOGFILE}
