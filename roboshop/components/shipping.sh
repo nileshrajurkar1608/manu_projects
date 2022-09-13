0d1e2c56268ed3b0cdef05e4e1a9d484602003e4
mvpc-11232 <mvp2612@hotmail.com>
2022-09-13 07:24:06 +0530

    Auto Push Commit

--- roboshop/components/shipping.sh
+++ roboshop/components/shipping.sh
@@ -7,23 +7,4 @@ source components/common.sh
 COMPONENT=shipping
 
 
-LOGFILE=/tmp/robot.log 
-
-echo -n "Installing Maven: "
-yum install maven -y &>> LOGFILE
-stat $? 
-
-# Created User 
-USER_SETUP 
-
-DOWNLOAD_AND_EXTRACT
-# Downloading and extract 
-
-echo -n "Generating the artifact :"
-cd /home/${FUSER}/${COMPONENT}
-mvn clean package   &>> LOGFILE
-mv target/shipping-1.0.jar shipping.jar
-stat $? 
-
-#Calling the svc setup 
-CONFIG_SVC
+MAVEN
