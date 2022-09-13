7c84b88a2eef795601a2614bc9856e98180748ff
mvpc-11232 <mvp2612@hotmail.com>
2022-09-13 06:23:45 +0530

    Auto Push Commit

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -27,5 +27,3 @@ stat $?
 
 
 echo -n "Uninstalling the password validate plugin: "
-echo 'uninstall plugin validate_password;' > /tmp/password-validate.sql 
-mysql --connect-expired-password -uroot -pRoboShop1 < /tmp/password-validate.sql
