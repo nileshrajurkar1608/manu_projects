11a1b7b3adb1c4b57ee45fb4c3189ae0e5af6c78
mvpc-11232 <mvp2612@hotmail.com>
2022-09-14 06:09:22 +0530

    Auto Push Commit

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -26,6 +26,6 @@ echo -n "Creating $COMPONENT Application user: "
 rabbitmqctl add_user roboshop roboshop123
 stat $? 
 
-echo -n "Configuring the $COMPONENET"
+echo -n "Configuring the $COMPONENT $FUSER permissions"
 rabbitmqctl set_user_tags roboshop administrator
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
