a120b1dd72f9f4f58f3d62c7f588c39f2d4e1a35
mvpc-11232 <mvp2612@hotmail.com>
2022-09-14 06:07:12 +0530

    Auto Push Commit

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -13,5 +13,5 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/sc
 stat $? 
 
 echo -n "Installing RabbitMQ: "
-yum install rabbitmq-server -y
+yum install rabbitmq-server -y  &>> ${LOGFILE} 
 stat $? 
