9c9e4e0095aa9412969273a7e21fe306b7dc2d4d
mvpc-11232 <mvp2612@hotmail.com>
2022-09-14 06:06:16 +0530

    Auto Push Commit

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -8,3 +8,5 @@ echo -n "Installing Erland dependency:"
 yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y &>> ${LOGFILE} 
 stat $? 
 
+echo -n "Configuring the Yum Repo for $COMPONENT:"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
