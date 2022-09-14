06b32a523d941fb5769615b3a15c717bacf6cee2
mvpc-11232 <mvp2612@hotmail.com>
2022-09-14 06:05:03 +0530

    Auto Push Commit

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -4,4 +4,5 @@ source components/common.sh
 
 COMPONENT=rabbitmq
 
-echo -n ""
+echo -n "Installing Erland dependency:"
+yum install https://github.com/rabbitmq/erlang-rpm/releases/download/v23.2.6/erlang-23.2.6-1.el7.x86_64.rpm -y
