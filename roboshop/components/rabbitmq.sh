35db9054132f1809237512f4185b368f3d7cc414
mvpc-11232 <mvp2612@hotmail.com>
2022-09-14 06:20:08 +0530

    Auto Push Commit

--- roboshop/components/rabbitmq.sh
+++ roboshop/components/rabbitmq.sh
@@ -22,7 +22,7 @@ systemctl start rabbitmq-server &>> ${LOGFILE}
 stat $? 
 
 rabbitmqctl list_users | grep roboshop  &>> ${LOGFILE} 
-if [ $? -eq 
+if [ $? -eq 0 ]; then 
 echo -n "Creating $COMPONENT Application user:" &>> ${LOGFILE} 
 rabbitmqctl add_user roboshop roboshop123 &>> ${LOGFILE} 
 stat $? 
