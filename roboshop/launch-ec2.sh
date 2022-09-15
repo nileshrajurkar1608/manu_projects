d8246cd93b56d070f4f7166b70f196614a973066
mvpc-11232 <mvp2612@hotmail.com>
2022-09-15 07:16:53 +0530

    Auto Push Commit

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -31,4 +31,5 @@ if [ "$1" == "all" ] ; then
     for component in catalogue cart shipping frontend mongodb payment rabbitmq redis mysql user; do 
         COMPONENT=$component
       #  calling function
+      create-server
 fi 
