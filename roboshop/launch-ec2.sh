8f0f02f838ecdda0967a0d12c77e99004258c5ac
mvpc-11232 <mvp2612@hotmail.com>
2022-09-15 06:18:49 +0530

    Auto Push Commit

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -3,3 +3,6 @@
 # AMI_ID="ami-00ff427d936335825"
 AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-CentOS7"  | jq '.Images[].ImageId' | sed -e 's/"//g')
 echo "The AMI which we are using is $AMI_ID"
+
+
+$ aws ec2 run-instances --image-id ami-xxxxxxxx --count 1 --instance-type t2.micro --key-name MyKeyPair --security-group-ids sg-903004f8 --subnet-id subnet-6e7f829e
