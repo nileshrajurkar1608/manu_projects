656a4eb94f39407a9a99ffa96aa9e0f5198691e7
mvpc-11232 <mvp2612@hotmail.com>
2022-09-15 06:43:08 +0530

    Auto Push Commit

--- roboshop/launch-ec2.sh
+++ roboshop/launch-ec2.sh
@@ -14,4 +14,4 @@ AMI_ID=$(aws ec2 describe-images  --filters "Name=name,Values=DevOps-LabImage-Ce
 SGID="sg-000671b0e1fb3d069"
 echo "The AMI which we are using is $AMI_ID"
 
-aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType: persistent,InstanceInterruptionBehavior:stop"}| jq 
+aws ec2 run-instances --image-id ${AMI_ID} --instance-type t3.micro  --security-group-ids ${SGID}  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" --instance-market-options "MarketType=spot, SpotOptions={SpotInstanceType: persistent,InstanceInterruptionBehavior=stop"}| jq 
